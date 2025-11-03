9575e25d6531b6e222904e153f5a4e9ba71764bb
xsq1234 <112300877+xsq12348@users.noreply.github.com>
2025-11-03 18:02:20 +0800

    Refactor Moon_Engine_Function.c for clarity and structure

--- Moon_Engine_Function.c
+++ Moon_Engine_Function.c
@@ -4,7 +4,6 @@ static TIMELOAD projectfps;
 static int fpsmax, fpsmax2;
 static IMAGE projectdoublebuffer;
 static POINT projectmousecoord;
-static int KEYSTATEbuffer[255];
 
 static LRESULT WINAPI WndPorc(HWND hwnd, UINT msgid, WPARAM wparam, LPARAM lparam)
 {
@@ -20,88 +19,6 @@ static LRESULT WINAPI WndPorc(HWND hwnd, UINT msgid, WPARAM wparam, LPARAM lpara
 		break;
 	}
 	return DefWindowProc(hwnd, msgid, wparam, lparam);
-}
-
-unsigned int Hash(char* text)
-{
-	if (text == NULL)return Error;
-	unsigned int length = (unsigned)strlen(text), hash = 0;
-	if (length <= 0)return Error;
-	for (unsigned int i = 0; i < length; i++)hash += text[i] * (i + 1);
-	return hash;
-}
-
-extern void TimeLoadInit(TIMELOAD* Timeload, int load)
-{
-	Timeload->time1 = NULL;
-	Timeload->time2 = NULL;
-	Timeload->timeload = load;
-	Timeload->timeswitch = 0;
-}
-
-extern int TimeLoad(TIMELOAD* Timeload, int mode)
-{
-	if (!mode)return FALSE;
-	else if (Timeload == NULL)
-		{
-			printf("Error:[TimeLoad函数错误!]存在空指针");
-			return Error;
-		}
-		if (!Timeload->timeswitch)
-		{
-			Timeload->time1 = clock();
-			if (Timeload->time1 > Timeload->time2 + Timeload->timeload)
-			{
-				Timeload->time2 = clock();
-				Timeload->timeswitch = TRUE;
-			}
-		}
-		else
-		{
-			Timeload->time2 = clock();
-			Timeload->timeswitch = FALSE;
-		}
-		return Timeload->timeswitch;
-}
-
-extern int MoonSleep(int timeload)
-{
-	Sleep(timeload);
-	return 0;
-}
-
-extern int CreateEntityIndex(PROJECTGOD* project, void* arrentity, char* nameid, int length)
-{
-	int index = NOTFOUND;
-	int hash = Hash(nameid) % ENTITYNUMBER;
-	if (project->entityindex[hash].entityindex == NULL)
-	{
-		if (hash != Error)
-		{
-			index = hash;
-			project->entityindex[index].entityindex = arrentity;
-			project->entityindex[index].nameid = nameid;
-			project->entityindex[index].length = length;
-		}
-		else printf("非法的名称[%s],无法通过这个字符串得到合法的索引", nameid);
-	}
-	else
-	{
-		printf("非法的名称[%s]，请换一个名字", nameid);
-		index = NOTFOUND;
-	}
-	return index;
-}
-
-extern int KeyState(int Key)
-{
-	int state = GetAsyncKeyState(Key);
-	if (state & 0x8000)
-	{
-		if (KEYSTATEbuffer[Key] == 0) { KEYSTATEbuffer[Key] = 1; return 1; }
-		return 0;
-	}
-	else { KEYSTATEbuffer[Key] = 0; return 0; }
 }
 
 extern HWND Window(LPCWSTR name,int window_coord_x, int window_coord_y, int window_width, int window_height)
@@ -158,6 +75,11 @@ static CREATETHREADFUNCTION(ProjectAttribute)
 
 extern void ProjectInit(PROJECTGOD* project, LPCWSTR project_name, int x, int y, int width, int height, int fps, void (*ProjectSetting_1)(PROJECTGOD*))
 {
+	if (x < 0 || y < 0)
+	{
+		x = (GetSystemMetrics(SM_CXSCREEN) - width) / 2;
+		y = (GetSystemMetrics(SM_CYSCREEN) - height) / 2;
+	}
 	project->hwnd = Window(project_name, x, y, width, height);
 	project->project_name = project_name;
 	project->window_coord_x = x;
