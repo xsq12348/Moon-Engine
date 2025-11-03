5fe164c8443baae870e8a304f8d7962626470d79
xsq1234 <112300877+xsq12348@users.noreply.github.com>
2025-11-03 18:02:48 +0800

    Implement utility functions for Moon project

---
+++ Moon_Utility_Function.c
@@ -0,0 +1,94 @@
+#include"Moon.h"
+
+static int KEYSTATEbuffer[255];
+
+extern unsigned int Hash(char* text)
+{
+	if (text == NULL)return Error;
+	unsigned int length = (unsigned)strlen(text), hash = 0;
+	if (length <= 0)return Error;
+	for (unsigned int i = 0; i < length; i++)hash += text[i] * (i + 1);
+	return hash;
+}
+
+extern void Music(LPCWSTR File)
+{
+    TCHAR cmd[255];
+    wsprintf(cmd, TEXT("open \%s\ alias music"), File);
+    mciSendString(TEXT("close music"), 0, 0, 0);
+    mciSendString(cmd, NULL, 0, NULL);
+    mciSendString(TEXT("play music"), NULL, 0, NULL);
+}
+
+extern void TimeLoadInit(TIMELOAD* Timeload, int load)
+{
+	Timeload->time1 = NULL;
+	Timeload->time2 = NULL;
+	Timeload->timeload = load;
+	Timeload->timeswitch = 0;
+}
+
+extern int TimeLoad(TIMELOAD* Timeload, int mode)
+{
+	if (!mode)return FALSE;
+	else if (Timeload == NULL)
+	{
+		printf("Error:[TimeLoad函数错误!]存在空指针");
+		return Error;
+	}
+	if (!Timeload->timeswitch)
+	{
+		Timeload->time1 = clock();
+		if (Timeload->time1 > Timeload->time2 + Timeload->timeload)
+		{
+			Timeload->time2 = clock();
+			Timeload->timeswitch = TRUE;
+		}
+	}
+	else
+	{
+		Timeload->time2 = clock();
+		Timeload->timeswitch = FALSE;
+	}
+	return Timeload->timeswitch;
+}
+
+extern int MoonSleep(int timeload)
+{
+	Sleep(timeload);
+	return 0;
+}
+
+extern int CreateEntityIndex(PROJECTGOD* project, void* arrentity, char* nameid, int length)
+{
+	int index = NOTFOUND;
+	int hash = Hash(nameid) % ENTITYNUMBER;
+	if (project->entityindex[hash].entityindex == NULL)
+	{
+		if (hash != Error)
+		{
+			index = hash;
+			project->entityindex[index].entityindex = arrentity;
+			project->entityindex[index].nameid = nameid;
+			project->entityindex[index].length = length;
+		}
+		else printf("非法的名称[%s],无法通过这个字符串得到合法的索引", nameid);
+	}
+	else
+	{
+		printf("非法的名称[%s]，请换一个名字", nameid);
+		index = NOTFOUND;
+	}
+	return index;
+}
+
+extern int KeyState(int Key)
+{
+	int state = GetAsyncKeyState(Key);
+	if (state & 0x8000)
+	{
+		if (KEYSTATEbuffer[Key] == 0) { KEYSTATEbuffer[Key] = 1; return 1; }
+		return 0;
+	}
+	else { KEYSTATEbuffer[Key] = 0; return 0; }
+}
