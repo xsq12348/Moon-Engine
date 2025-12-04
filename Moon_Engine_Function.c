87a8c34caa025ef5f1de4ba88c98a11d92e2adae
xsq1234 <112300877+xsq12348@users.noreply.github.com>
2025-12-05 00:01:16 +0800

    Update Moon Engine version and refactor functions

--- Moon_Engine_Function.c
+++ Moon_Engine_Function.c
@@ -1,6 +1,6 @@
 #include"Moon.h"
 
-static char Moon_Engine_VSn[4] = { 1,1,7,0 };
+static char Moon_Engine_VSn[4] = { 1,1,8,0 };
 static TIMELOAD projectfps;
 static int fpsmax, fpsmax2;
 static IMAGE projectdoublebuffer;
@@ -252,3 +252,4 @@ extern int  ProjectFindEntityAllNumber(PROJECTGOD* project)
 	printf("\n[ProjectFindEntityAllNumber函数]进入成功!\n统计到的实体总数为[%d]\n", all_number);
 	return all_number;
 }
+
