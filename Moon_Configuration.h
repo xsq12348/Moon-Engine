619adf02cc3d2f7e3e3ceeaf590e86a92d1e7419
xsq1234 <112300877+xsq12348@users.noreply.github.com>
2025-12-05 17:31:16 +0800

    Update Moon_Configuration.h

--- Moon_Configuration.h
+++ Moon_Configuration.h
@@ -1,4 +1,5 @@
-#define OPEN_SDL 0
-#define MOONMANYENTITY      0
-#define MOONSTANDARDENTITY  1
-#define MOONFEWENTITY       0
+#define OPEN_SDL                0
+#define MOONMANYENTITY          0
+#define MOONSTANDARDENTITY      0
+#define MOONFEWENTITY           1
+#define MOONCENTRALDISPATCHER   0
