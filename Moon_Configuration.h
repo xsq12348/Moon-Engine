c590defa4d9d6cce5f98b44d9f592fde10a1efcb
xsq1234 <112300877+xsq12348@users.noreply.github.com>
2026-01-29 22:45:26 +0800

    Change OPEN_SDL definition to 0

--- Moon_Configuration.h
+++ Moon_Configuration.h
@@ -1,6 +1,7 @@
 #pragma once
-#define OPEN_SDL                1
+#define OPEN_SDL                0
 #define MOONMANYENTITY          0
 #define MOONSTANDARDENTITY      1
 #define MOONFEWENTITY           0
 #define MOONCOMPATIBLE			0
+
